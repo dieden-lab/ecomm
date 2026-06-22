29b5bb10495d51fb571a7b1b6029242db5532c3e
dieden-lab <diego.randazzo@dentsu.com>
2026-06-22 12:41:02 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -6,7 +6,7 @@ export default function middleware(request) {
   const auth = request.headers.get('authorization');
 
   const validUser = 'merkle';
-  const validPass = 'elkrem';
+  const validPass = 'demo2025';
   const expected = 'Basic ' + btoa(`${validUser}:${validPass}`);
 
   if (auth !== expected) {
